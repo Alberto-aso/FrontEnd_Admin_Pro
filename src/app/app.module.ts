f5343501ae9eb15aa2e44e7402ac895afebd9e32
Alberto Sanchez Ortiz <alberto.asow@gmail.com>
2023-02-24 20:40:19 -0600

    Inicio aplicacion Admin Pro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { Page404Component } from './pageNotFound/page404/page404.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    Page404Component,
+    DashboardComponent,
+    BreadcrumbsComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
